fb6657165d83f30a37262d24531f71c82505b6b5
libchaos <libchaos01@163.com>
2015-11-17 10:45:33 +0800

    marked render

--- client/Blog.jsx
+++ client/Blog.jsx
@@ -1,22 +1,21 @@
 Blog = React.createClass({
   getInitialState(){
     return {
-      posts: [
-        {
-          "id": 1,
-          "post_name": "1-beauty",
-          "title": "第一个 METEOR 应用",
-          "date": "2015年10月1号"
-        },
-        {
-          "id": 2,
-          "post_name": "2-beast",
-          "title": "狼人传说",
-          "date": "2015年10月8号"
-        }
-      ]
+      posts: []
     };
   },
+  componentWillMount() {
+    let that = this;
+
+    Meteor.call('/blog/getPost', "posts", function(err, res){
+      if (err) {
+        console.log(`The post ${postName} does not exist!`);
+        return;
+      }
+      that.setState({ posts: JSON.parse(res) });
+    });
+  },
+
 
   render() {
     return (
--- client/lib/app.browserify.js
+++ client/lib/app.browserify.js
@@ -1,6 +1,8 @@
 var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 mui = require('material-ui');
+marked = require('marked');
+hljs = require("highlight.js");
 ReactRouter = require("react-router");
 
 History = require("history");
